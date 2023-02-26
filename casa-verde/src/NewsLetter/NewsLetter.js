88ead5f005d360675875acbb8ec00367990bf77b
Camila <camilacaleones@dcc.ufrj.br>
2023-02-26 18:25:48 -0300

    adicionado parte da newsletter

--- casa-verde/src/NewsLetter/NewsLetter.js
+++ casa-verde/src/NewsLetter/NewsLetter.js
@@ -1,7 +1,36 @@
+import planta from './imagem-hero.png'
+import mail from './mail.png'
+
+import './NewsLetter.css'
 export default function AssinaturaNewsletter() {
     return (
-         <div>
-               
-         </div>
+        <div className='News'>
+            <div className='text'>
+                <p className='first-text'>Sua casa com as</p>
+                <h1>melhores plantas</h1> 
+                <p className='sub-text'>Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-lo uma pessoa mais feliz no seu dia a dia. Entre com seu e-mail e assine nossa newsletter para saber das novidades da marca.</p>
+            
+                <div className="mail-box">
+                    <img
+                    className="mail-icon"
+                    src={mail}
+                    alt="mail"
+                    />
+                    <input type="text" className='mail-input' placeholder='Insira seu e-mail'/>
+                    <button type="button">Assinar NewsLetter</button>
+                </div>
+
+                
+
+            </div>
+
+            <div className='img'>
+                <img
+                className="planta"
+                src={planta}
+                alt="planta"
+                />
+            </div>
+        </div>
      );
 }
